368cfc67bc0c2aaddda6f6d064b0d4013ca86ad4
Ihor Panchenko <ipanchenko91@gmail.com>
2024-09-20 14:51:48 +0200

    Check if a deliveryOptionId exists

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import { validDeliveryOption } from "./deliveryOptions.js";
+
 export let cart = JSON.parse(localStorage.getItem("cart"));
 
 if (!cart) {
@@ -62,6 +64,14 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchingItem = cartItem;
     }
   });
+
+  if (!matchingItem) {
+    return;
+  }
+
+  if (!validDeliveryOption(deliveryOptionId)) {
+    return;
+  }
 
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
